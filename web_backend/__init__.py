c06a81396612ffe444c065c25bb92816f77e2232
shen <1593995275@qq.com>
2021-10-09 23:18:44 +0800

    增加环境变量功能查询支持 -001

--- web_backend/__init__.py
+++ web_backend/__init__.py
@@ -1,7 +1,7 @@
 import hashlib
-
 from flask import Flask, request, Response
 import settings
+from web_backend.env_variable.views import env_variable
 from web_backend.user.view import user
 import json
 from mysql.pymysql import SQLMysql
@@ -9,49 +9,49 @@ from mysql.pymysql import SQLMysql
 app = Flask(__name__)
 
 
-lists = ['/login', '/resigter']
+lists = ['/login']
 
 
-@app.before_request
-def interceptor():
-    url = request.path
-    for i in range(len(lists)):
-        if lists[i] == url:
-            return
-    uuid = request.cookies.get('uuid', None)
-    username = request.cookies.get('username', None)
-    if uuid is None or username is None:
-        data = {
-            "object": None,
-            "msg": "用户未登录！",
-            "code": 9200,
-            "result": False,
-            "status": "success"
-        }
-        return Response(json.dumps(data), content_type='application/json')
-    # 判断用户数据是否正确
-    s = SQLMysql()
-    sql = "select u_id, u_password, u_salt from user_info where u_name=%s"
-    is_null = s.query_one(sql, [username, ])
-    if is_null is None:
-        data = {
-            "object": None,
-            "msg": "用户未登录！",
-            "code": 9201,
-            "result": False,
-            "status": "success"
-        }
-        return Response(json.dumps(data), content_type='application/json')
-    user_uuid = hashlib.md5((str(is_null[0]) + is_null[2]).encode('utf-8')).hexdigest()
-    if user_uuid != uuid:
-        data = {
-            "object": None,
-            "msg": "用户未登录！",
-            "code": 9202,
-            "result": False,
-            "status": "success"
-        }
-        return Response(json.dumps(data), content_type='application/json')
+# @app.before_request
+# def interceptor():
+#     url = request.path
+#     for i in range(len(lists)):
+#         if lists[i] == url:
+#             return
+#     uuid = request.cookies.get('uuid', None)
+#     username = request.cookies.get('username', None)
+#     if uuid is None or username is None:
+#         data = {
+#             "object": None,
+#             "msg": "用户未登录！",
+#             "code": 9200,
+#             "result": False,
+#             "status": "success"
+#         }
+#         return Response(json.dumps(data), content_type='application/json')
+#     # 判断用户数据是否正确
+#     s = SQLMysql()
+#     sql = "select u_id, u_password, u_salt from user_info where u_name=%s"
+#     is_null = s.query_one(sql, [username, ])
+#     if is_null is None:
+#         data = {
+#             "object": None,
+#             "msg": "用户未登录！",
+#             "code": 9201,
+#             "result": False,
+#             "status": "success"
+#         }
+#         return Response(json.dumps(data), content_type='application/json')
+#     user_uuid = hashlib.md5((str(is_null[0]) + is_null[2]).encode('utf-8')).hexdigest()
+#     if user_uuid != uuid:
+#         data = {
+#             "object": None,
+#             "msg": "用户未登录！",
+#             "code": 9202,
+#             "result": False,
+#             "status": "success"
+#         }
+#         return Response(json.dumps(data), content_type='application/json')
 
 
 def create_app():
@@ -59,4 +59,5 @@ def create_app():
     app.config.from_object(settings.Development)
     # 注册蓝图
     app.register_blueprint(user)
+    app.register_blueprint(env_variable)
     return app
--- web_backend/env_variable/views.py
+++ web_backend/env_variable/views.py
@@ -1,17 +1,116 @@
 from flask import Blueprint, request, Response
 import json
+import re
+from mysql.pymysql import SQLMysql
 
 env_variable = Blueprint('env_variable', __name__)
 
 
 @env_variable.route('/env', methods=['GET', 'POST'])
-def env_variable():
+def env_var():
     if request.method == "GET":
-        page = request.json.get('page', 1)
-        limit = request.json.get('limit', 10)
-        variable = request.json.get('variable', None)
-        context = request.json.get('context', None)
+        page = request.args.get('page', 1)
+        limit = request.args.get('limit', 10)
+        variable = request.args.get('variable', None)
+        context = request.args.get('context', None)
         # 处理
+        pattern = re.compile(r'^[\d]{1,3}$')
+        page = pattern.search(str(page))
+        limit = pattern.search(str(limit))
+        if page is None or limit is None:
+            data = {
+                "object": None,
+                "msg": "参数非法",
+                "code": 7998,
+                "result": False,
+                "status": "success"
+            }
+            return Response(json.dumps(data), content_type='application/json')
+        else:
+            s = SQLMysql()
+            s_sql = "select group_id from jk_vgroups where group_name=%s"
+            s_all = "select group_id from jk_vgroups"
+            sql = "select v_id, v_name, v_data, group_id from jk_variable where is_delete=0 and group_id=%s and v_data like %s"
+            if variable is None:
+                group_ids = s.query_all(s_all)
+                if not group_ids:
+                    data = {
+                        "object": None,
+                        "msg": "暂无数据",
+                        "code": 7994,
+                        "result": True,
+                        "status": "success"
+                    }
+                    return Response(json.dumps(data), content_type='application/json')
+                else:
+                    sql = "select v.v_id, v.v_name, v.v_data, g.group_name from jk_vgroups g, jk_variable v WHERE v.group_id=g.group_id"
+                    l_n = s.query_all(sql)
+                    if not l_n:
+                        data = {
+                            "object": None,
+                            "msg": "暂无数据",
+                            "code": 7993,
+                            "result": True,
+                            "status": "success"
+                        }
+                        return Response(json.dumps(data), content_type='application/json')
+                    else:
+                        lists_all = []
+                        for i in range(len(l_n)):
+                            v_id, v_name, v_data, group_name = l_n[i]
+                            lists_all.append({
+                                "id": v_id,
+                                "name": v_name,
+                                "data": v_data,
+                                "group_name": group_name
+                            })
+                        data = {
+                            "object": lists_all,
+                            "msg": "查询成功",
+                            "code": 7992,
+                            "result": True,
+                            "status": "success"
+                        }
+                        return Response(json.dumps(data), content_type='application/json')
+            else:
+                group_id = s.query_one(s_sql, [str(variable), ])
+                if group_id is None:
+                    data = {
+                        "object": None,
+                        "msg": "无相关结果",
+                        "code": 7997,
+                        "result": False,
+                        "status": "success"
+                    }
+                    return Response(json.dumps(data), content_type='application/json')
+                li = s.query_all(sql, [group_id, ('%' + context + '%'), ])
+                if not li:
+                    data = {
+                        "object": None,
+                        "msg": "暂无数据",
+                        "code": 7996,
+                        "result": True,
+                        "status": "success"
+                    }
+                    return Response(json.dumps(data), content_type='application/json')
+                else:
+                    list_s = []
+                    for i in range(len(li)):
+                        v_id, v_name, v_data, group_id = li[i]
+                        list_s.append({
+                            "id": v_id,
+                            "name": v_name,
+                            "data": v_data,
+                            "group_name": variable
+                        })
+                    data = {
+                        "object": list_s,
+                        "msg": "查询成功",
+                        "code": 7995,
+                        "result": True,
+                        "status": "success"
+                    }
+                    return Response(json.dumps(data), content_type='application/json')
     elif request.method == "POST":
         variable = request.json.get('variable', "testing_env")
         status = request.json.get('status', 0)

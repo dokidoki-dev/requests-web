505d5a703160748dadec85ded4f8447fa97b9e86
shen <1593995275@qq.com>
2021-11-07 12:04:12 +0800

    添加用户模块日志记录

--- web_backend/logger_text/logger_text.py
+++ web_backend/logger_text/logger_text.py
@@ -8,7 +8,7 @@ Log_level = settings.Log_level
 # ERROR日志
 logger.add(
     "%s/logs/{time:YYYYMMDD}-error.log" % base_dir,
-    format="{time:YYYY-MM-DD HH:mm:ss.SSS} | {level} | file:{file.path} | line:{line} | function:{function} | 消息: {message}",
+    format="{time:YYYY-MM-DD HH:mm:ss.SSS} | {level} | file:{file.path} | line:{line} | function:{function} | msg: {message}",
     filter=lambda x: True if x["level"].name == "ERROR" else False,
     rotation="00:00", retention=7, level='ERROR', encoding='utf-8',
     backtrace=True, diagnose=True
@@ -16,7 +16,7 @@ logger.add(
 # INFO 日志
 logger.add(
     "%s/logs/{time:YYYYMMDD}.log" % base_dir,
-    format="{time:YYYY-MM-DD HH:mm:ss.SSS} | {level} | file:{file} | line:{line} | function:{function} | 消息: {message}",
+    format="{time:YYYY-MM-DD HH:mm:ss.SSS} | {level} | file:{file} | line:{line} | function:{function} | msg: {message}",
     filter=lambda x: False if x["level"].name == "ERROR" else True,
     rotation="00:00", retention=7, level=Log_level, encoding='utf-8',
     backtrace=True, diagnose=True
--- web_backend/user/views.py
+++ web_backend/user/views.py
@@ -4,8 +4,12 @@ import mysql.pymysql as pymysql
 import re
 import hashlib
 from web_backend.user import logic
+from web_backend.logger_text.logger_text import log
 
 user = Blueprint('user', __name__)
+
+# 日志处理
+logger = log()
 
 
 @user.route('/login', methods=['POST'])
@@ -18,29 +22,39 @@ def login():
     }
     # 处理没有传参的问题
     if not request.json:
+        logger.debug("request.json:" + str(request.json))
+        logger.info("返回信息" + str(data))
         return Response(json.dumps(data), content_type='application/json')
     username = str(request.json.get('username', None)).lower()
     password = request.json.get('password', None)
     if username is None or password is None:
+        logger.debug("request.json:" + str(request.json))
+        logger.info("返回信息" + str(data))
         return Response(json.dumps(data), content_type='application/json')
     # 查询用户是否存在
     s = pymysql.SQLMysql()
     sql = "select u_name, u_password, u_salt, is_active, is_delete, u_id from user_info where u_name = %s"
+    logger.debug("select u_name, u_password, u_salt, is_active, is_delete, u_id from user_info where u_name=%s" % username)
     is_null = s.query_one(sql, [username, ])
+    logger.debug("查询用户信息：" + str(is_null))
     if is_null is None:
         data["code"] = 9998
         data["msg"] = "用户名或密码错误"
+        logger.info("返回信息" + str(data))
         return Response(json.dumps(data), content_type='application/json')
     if is_null[3] == 0:
         data["code"] = 9997
         data["msg"] = "账户已禁用，禁止登录！"
+        logger.info("返回信息" + str(data))
         return Response(json.dumps(data), content_type='application/json')
     if is_null[4] == 1:
         data["code"] = 9996
         data["msg"] = "账户已注销，禁止登录！"
+        logger.info("返回信息" + str(data))
         return Response(json.dumps(data), content_type='application/json')
     # 加密用户密码
     password = hashlib.sha256((password + is_null[2]).encode('utf-8')).hexdigest()
+    logger.debug("密码比对：" + "用户输入密码：" + str(password) + " 用户保存的密码：" + str(is_null[1]) + " 比对结果：" + str(password == is_null[1]))
     # 比对用户密码
     if password == is_null[1]:
         data["code"] = 9000
@@ -51,10 +65,13 @@ def login():
         uuid = hashlib.md5((str(is_null[5]) + is_null[2]).encode('utf-8')).hexdigest()
         response.set_cookie('uuid', uuid, max_age=86400, domain='127.0.0.1')
         response.set_cookie('username', is_null[0])
+        logger.debug("uuid:" + str(uuid))
+        logger.info("返回信息" + str(data))
         return response
     else:
         data["code"] = 9995
         data["msg"] = "账号或密码错误！"
+        logger.info("返回信息" + str(data))
         return Response(json.dumps(data), content_type='application/json')
 
 
@@ -68,6 +85,8 @@ def res_user():
     }
     # 处理没有传参的问题
     if not request.json:
+        logger.debug("request.json: " + str(request.json))
+        logger.info("返回信息" + str(data))
         return Response(json.dumps(data), content_type='application/json')
     # 只支持注册普通用户
     username = request.json.get('username', "").lower()
@@ -82,18 +101,23 @@ def res_user():
     if username is None or password is None or phone is None:
         data["code"] = 9994
         data["msg"] = "填写的用户信息校验不通过"
+        logger.info("返回信息" + str(data))
         return Response(json.dumps(data), content_type='application/json')
     # 查询用户是否存在
     s = pymysql.SQLMysql()
     sql = "select u_name, u_password, u_salt, is_delete from user_info where u_name = %s"
+    logger.debug("select u_name, u_password, u_salt, is_delete from user_info where u_name = %s" % username[0])
     is_null = s.query_one(sql, [username[0], ])
+    logger.debug("查询信息：" + str(is_null))
     if is_null:
         if is_null[3] == 1:
             data["msg"] = "当前用户已注销，不支持重新注册"
             data["code"] = 9993
+            logger.info("返回信息" + str(data))
             return Response(json.dumps(data), content_type='application/json')
         data["msg"] = "当前用户已注册"
         data["code"] = 9992
+        logger.info("返回信息" + str(data))
         return Response(json.dumps(data), content_type='application/json')
     # 注册
     # 加密密码
@@ -101,15 +125,18 @@ def res_user():
     salt = logic.hash_salt()
     password = hashlib.sha256((password[0] + salt).encode('utf-8')).hexdigest()
     sql = "insert into user_info (u_name, u_password, u_salt, u_phone, create_time) values (%s, %s, %s, %s, now())"
+    logger.debug("insert into user_info (u_name, u_password, u_salt, u_phone, create_time) values ({}, {}, {}, {}, now())".format(username[0], password, salt, phone[0]))
     is_ok = s.create_one(sql, [(username[0]), password, salt, (phone[0]), ])
     if is_ok:
         data["msg"] = "账户注册成功"
         data["code"] = 9991
         data["result"] = True
+        logger.info("返回信息" + str(data))
         return Response(json.dumps(data), content_type='application/json')
     else:
         data["msg"] = "未知异常"
         data["code"] = 9990
+        logger.info("返回信息" + str(data))
         return Response(json.dumps(data), content_type='application/json')
 
 
@@ -123,6 +150,7 @@ def logout():
     }
     username = request.cookies.get('username', None)
     if username is None:
+        logger.info("返回信息" + str(data))
         return Response(json.dumps(data), content_type='application/json')
     data["code"] = 9898
     data["msg"] = "用户已退出！"
@@ -130,6 +158,7 @@ def logout():
     response = Response(json.dumps(data), content_type='application/json')
     response.delete_cookie('uuid')
     response.delete_cookie('username')
+    logger.info("返回信息" + str(data))
     return response
 
 
@@ -147,19 +176,26 @@ def user_list():
     # 判断是否管理员
     u_name = request.cookies.get('username')
     select = "select is_admin from user_info where u_name=%s and is_delete=0 and is_active=1"
+    logger.debug("select is_admin from user_info where u_name={} and is_delete=0 and is_active=1".format(u_name))
     s = pymysql.SQLMysql()
     is_null = s.query_one(select, [u_name, ])
+    logger.debug("查询结果：" + str(is_null))
     if is_null is None:
         data["code"] = 9398
+        logger.info("返回信息" + str(data))
         return Response(json.dumps(data), content_type='application/json')
     if is_null[0] == 1:
         sql = "select u_name, u_phone, is_active from user_info where is_delete=0 limit %s, %s"
+        logger.debug("select u_name, u_phone, is_active from user_info where is_delete=0 limit {}, {}".format((page-1), limit))
         list_n = s.query_all(sql, [(page - 1), limit, ])
+        logger.debug("查询结果：" + str(list_n))
         if not list_n:
+            # 理论上不会出现此种情况
             data["is_admin"] = True
             data["code"] = 9199
             data["msg"] = "查询成功"
             data["result"] = True
+            logger.info("返回信息" + str(data))
             return Response(json.dumps(data), content_type='application/json')
         context = []
         for i in range(len(list_n)):
@@ -175,28 +211,34 @@ def user_list():
         data["msg"] = "查询成功"
         data["code"] = 9198
         data["result"] = True
+        logger.info("返回信息" + str(data))
         return Response(json.dumps(data), content_type='application/json')
     elif is_null[0] == 0:
-        sql = "select u_name, u_phone from user_info where is_delete=0 and is_active=1 and u_name=%s"
+        sql = "select u_name, u_phone, is_active from user_info where is_delete=0 and is_active=1 and u_name=%s"
+        logger.debug("select u_name, u_phone, is_active from user_info where is_delete=0 and is_active=1 and u_name={}".format(u_name))
         list_p = s.query_one(sql, [u_name, ])
         if list_p is None:
-            data["is_admin"] = True
+            # 理论上不会出现此种情况
+            data["is_admin"] = False
             data["msg"] = "查询成功"
             data["code"] = 9199
             data["result"] = True
+            logger.info("返回信息" + str(data))
             return Response(json.dumps(data), content_type='application/json')
-        username, phone = list_p
+        username, phone, is_active = list_p
         data = {
             "object": {
                 "username": username,
                 "password": "********",
-                "phone": phone
+                "phone": phone,
+                "active": True if is_active == 1 else False
             },
             "is_admin": False,
             "msg": "查询成功",
             "code": 9197,
             "result": True
         }
+        logger.info("返回信息" + str(data))
         return Response(json.dumps(data), content_type='application/json')
 
 
@@ -210,6 +252,8 @@ def user_delete():
     }
     # 处理没有传参的问题
     if not request.json:
+        logger.debug("request.json: " + str(request.json))
+        logger.info("返回信息" + str(data))
         return Response(json.dumps(data), content_type='application/json')
     username = request.json.get('username', "")
     pattern = re.compile(r'^[a-zA-Z][a-zA-Z0-9_]{4,15}$')
@@ -217,39 +261,49 @@ def user_delete():
     if username is None:
         data["msg"] = "填写的信息校验不通过"
         data["code"] = 9599
+        logger.info("返回信息" + str(data))
         return Response(json.dumps(data), content_type='application/json')
     s = pymysql.SQLMysql()
     # 查询当前用户是否存在
     sl = "select username from user_info where u_name=%s and is_delete=0"
-    is_null = s.query_one(sl, [username[0]])
+    logger.debug("select username from user_info where u_name={} and is_delete=0".format(username[0]))
+    is_null = s.query_one(sl, [username[0], ])
+    logger.debug("查询结果：" + str(is_null))
     if is_null is None:
         data["msg"] = "用户删除失败"
         data["code"] = 9598
+        logger.info("返回信息" + str(data))
         return Response(json.dumps(data), content_type='application/json')
     # 判断是否管理员
     user_name = request.cookies.get('username')
     select = "select is_admin from user_info where u_name=%s and is_delete=0"
     sql = "update user_info set is_delete=1, delete_time=now() where u_name=%s and is_delete=0"
+    logger.debug("select is_admin from user_info where u_name={} and is_delete=0".format(user_name))
     ok = s.query_one(select, [user_name, ])
     if ok is None:
         data["msg"] = "参数非法"
         data["code"] = 9597
+        logger.info("返回信息" + str(data))
         return Response(json.dumps(data), content_type='application/json')
     if ok[0] == 1:
         # 说明是管理员
+        logger.debug("update user_info set is_delete=1, delete_time=now() where u_name={} and is_delete=0".format(username[0]))
         nok = s.update_one(sql, [(username[0]), ])
         if nok:
             data["msg"] = "用户删除成功!"
             data["code"] = 9596
             data["result"] = True
+            logger.info("返回信息" + str(data))
             return Response(json.dumps(data), content_type='application/json')
         else:
             data["msg"] = "未知异常!"
             data["code"] = 9595
+            logger.info("返回信息" + str(data))
             return Response(json.dumps(data), content_type='application/json')
     else:
         data["msg"] = "权限不足"
         data["code"] = 9594
+        logger.info("返回信息" + str(data))
         return Response(json.dumps(data), content_type='application/json')
 
 
@@ -263,6 +317,8 @@ def user_update():
     }
     # 处理没有传参的问题
     if not request.json:
+        logger.debug("request.json: " + str(request.json))
+        logger.info("返回信息" + str(data))
         return Response(json.dumps(data), content_type='application/json')
     username = request.json.get('username', "")
     password = request.json.get('password', "")
@@ -279,6 +335,7 @@ def user_update():
     if username is None or password is None or phone is None or is_active is None:
         data["msg"] = "填写的用户信息校验不通过"
         data["code"] = 9699
+        logger.info("返回信息" + str(data))
         return Response(json.dumps(data), content_type='application/json')
     # 处理密码
     salt = logic.hash_salt()
@@ -288,34 +345,43 @@ def user_update():
     # 判断是否管理员
     user_name = request.cookies.get('username')
     select = "select is_admin from user_info where u_name=%s and is_delete=0"
+    logger.debug("select is_admin from user_info where u_name={} and is_delete=0".format(user_name))
     is_null = s.query_one(select, [user_name, ])
+    logger.debug("查询结果：" + str(is_null))
     if is_null is None:
         data["msg"] = "参数非法"
         data["code"] = 9099
+        logger.info("返回信息" + str(data))
         return Response(json.dumps(data), content_type='application/json')
     if is_null[0] == 1:
         # 说明是管理员
+        logger.debug("update user_info set u_password={}, u_phone={}, is_active={}, u_salt={}, modfiy_time=now() where u_name={} and is_delete=0".format(password, phone[0], is_active[0], salt, username[0]))
         ok = s.update_one(sql_s, [password, (phone[0]), (is_active[0]), salt, (username[0]), ])
         if ok:
             data["msg"] = "修改成功"
             data["code"] = 9697
             data["result"] = True
+            logger.info("返回信息" + str(data))
             return Response(json.dumps(data), content_type='application/json')
         else:
             data["msg"] = "未知异常"
             data["code"] = 9696
+            logger.info("返回信息" + str(data))
             return Response(json.dumps(data), content_type='application/json')
     # 普通用户更新自己信息
     sql_p = "update user_info set u_password=%s, u_phone=%s, u_salt=%s, modfiy_time=now() where u_name=%s and is_delete=0 and is_active=1"
+    logger.debug("update user_info set u_password=%s, u_phone=%s, u_salt=%s, modfiy_time=now() where u_name=%s and is_delete=0 and is_active=1".format(password, phone[0], salt, user_name))
     ok = s.update_one(sql_p, [password, (phone[0]), salt, user_name, ])
     if ok:
         data["msg"] = "修改成功"
         data["code"] = 9695
         data["result"] = True
+        logger.info("返回信息" + str(data))
         return Response(json.dumps(data), content_type='application/json')
     else:
         data["msg"] = "未知异常"
         data["code"] = 9694
+        logger.info("返回信息" + str(data))
         return Response(json.dumps(data), content_type='application/json')
 
 
@@ -330,6 +396,8 @@ def user_updatepd():
     }
     # 处理没有传参的问题
     if not request.json:
+        logger.debug("request.json: " + str(request.json))
+        logger.info("返回信息" + str(data))
         return Response(json.dumps(data), content_type='application/json')
     password_old = request.json.get('password_old', "")
     password_new = request.json.get('password_new', "")
@@ -340,28 +408,36 @@ def user_updatepd():
     if password_old is None or password_new is None or not username:
         data["msg"] = "参数非法"
         data["code"] = 5999
+        logger.info("返回信息" + str(data))
         return Response(json.dumps(data), content_type='application/json')
     s = pymysql.SQLMysql()
     sql_old = "select u_password, u_salt  from user_info where u_name=%s"
+    logger.debug("select u_password, u_salt  from user_info where u_name={}".format(username))
     is_null = s.query_one(sql_old, [username, ])
+    logger.debug("查询结果：" + str(is_null))
     # 比对密码
     password_old = hashlib.sha256(password_old[0] + is_null[1]).hexdigest()
+    logger.debug("校验密码：" + "用户输入的旧密码：" + str(password_old) + " 数据库中的旧密码：" + str(is_null[0]) + " 校验结果：" + password_old == is_null[0])
     if password_old == is_null[0]:
         # 处理新密码
         salt = logic.hash_salt()
         password_new = hashlib.sha256(password_new[0] + salt).hexdigest()
         sql_new = "update user_info set u_password=%s, u_salt=%s where u_name=%s"
+        logger.debug("update user_info set u_password={}, u_salt={} where u_name={}".format(password_new, salt, username))
         ok = s.update_one(sql_new, [password_new, salt, username, ])
         if ok:
             data["msg"] = "修改成功"
             data["code"] = 5998
             data["result"] = True
+            logger.info("返回信息" + str(data))
             return Response(json.dumps(data), content_type='application/json')
         else:
             data["msg"] = "未知异常"
             data["code"] = 5997
+            logger.info("返回信息" + str(data))
             return Response(json.dumps(data), content_type='application/json')
     else:
         data["msg"] = "密码错误"
         data["code"] = 5996
+        logger.info("返回信息" + str(data))
         return Response(json.dumps(data), content_type='application/json')

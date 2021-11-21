ffa9436261355c47658baa3a7bda66e9e4d87117
shen <1593995275@qq.com>
2021-11-21 18:47:05 +0800

    增加自动任务执行模块部分功能 ---001

--- web_backend/task_auto/customt_hread.py
+++ web_backend/task_auto/customt_hread.py
@@ -1,13 +1,16 @@
 import threading
 from web_backend.auto_queue.t_queue import auto_queue
+from web_backend.logger_text.logger_text import log
+from mysql.pymysql import SQLMysql
 
-
+logger = log()
 q = auto_queue()
 
 
 # 自定义线程
 class CustomThread(threading.Thread):
     def __init__(self, queue, **kwargs):
+        self.sql = "update jk_testcase set status=%s, sub_status=%s, result_code=%s, a_result_data=%s, a_status=%s, result_data=%s where case_id=%s"
         super(CustomThread, self).__init__(**kwargs)
         self.__queue = queue
 
@@ -20,6 +23,15 @@ class CustomThread(threading.Thread):
             # item[0](*item[1:])
             # 告诉队列，任务已完成
             self.__queue.task_done()
+
+    def __update_status(self, case_id) -> bool:
+        logger.debug("update jk_testcase set status=%s, sub_status=%s, result_code=%s, a_result_data=%s, a_status=%s, result_data=%s where case_id={}".format(case_id))
+        s = SQLMysql()
+        ok = s.update_one(self.sql, [case_id, ])
+        if ok:
+            return True
+        else:
+            return False
 
 
 def start_thread() -> None:

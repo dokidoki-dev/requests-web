285d9fee8a3fbef7ca8cbd99df7770cc1f6d9f66
shen <1593995275@qq.com>
2022-01-22 23:41:13 +0800

    用例列表数据查询逻辑优化

--- web_backend/t_cases/views.py
+++ web_backend/t_cases/views.py
@@ -646,7 +646,15 @@ def t_lists():
             group_name = group_id[0]
     sql = "select t.sort, t.case_id, t.case_name, t.method, t.path, t.url, t.params, t.status, t.is_assert, t.is_rely_on, t.rely_id, t.header, t.request_data, c.group_name from jk_testcase as t INNER JOIN jk_cgroups as c ON t.group_id = c.group_id  where t.group_id=%s or t.case_id=%s or t.case_name like %s limit %s, %s"
     logger.debug("select t.sort, t.case_id, t.case_name, t.method, t.path, t.url, t.params, t.status, t.is_assert, t.is_rely_on, t.rely_id, t.header, t.request_data, c.group_name from jk_testcase as t INNER JOIN jk_cgroups as c ON t.group_id = c.group_id  where t.group_id={} or t.case_id={} or t.case_name like {} limit {}, {}".format(group_name, case_id, ('%' + str(case_name) + '%'), (page - 1), limit))
-    li = s.query_all(sql, [group_name, case_id, ('%' + str(case_name) + '%'), (page - 1), limit, ])
+    queryp = [group_name, case_id, ('%' + str(case_name) + '%'), (page - 1), limit, ]
+    if case_id is None:
+        if case_name is None:
+            sql = "select t.sort, t.case_id, t.case_name, t.method, t.path, t.url, t.params, t.status, t.is_assert, t.is_rely_on, t.rely_id, t.header, t.request_data, c.group_name from jk_testcase as t INNER JOIN jk_cgroups as c ON t.group_id = c.group_id  where t.group_id=%s  limit %s, %s"
+            queryp = [group_name, (page - 1), limit, ]
+        else:
+            sql = "select t.sort, t.case_id, t.case_name, t.method, t.path, t.url, t.params, t.status, t.is_assert, t.is_rely_on, t.rely_id, t.header, t.request_data, c.group_name from jk_testcase as t INNER JOIN jk_cgroups as c ON t.group_id = c.group_id  where t.group_id=%s or t.case_name like %s limit %s, %s"
+            queryp = [group_name, ('%' + str(case_name) + '%'), (page - 1), limit, ]
+    li = s.query_all(sql, queryp)
     logger.debug("查询信息：" + str(li))
     if not li:
         data["msg"] = "暂无数据"

a5206bdc505669184dcb70c29307e10469cd5d27
shen <1593995275@qq.com>
2022-01-29 20:19:00 +0800

    修复用例查询bug

--- web_backend/t_cases/views.py
+++ web_backend/t_cases/views.py
@@ -674,7 +674,7 @@ def t_lists():
             return Response(json.dumps(data), content_type='application/json')
         else:
             group_name = group_id[0]
-    sql = "select t.sort, t.case_id, t.case_name, t.method, t.path, t.url, t.params, t.status, t.is_assert, t.is_rely_on, t.rely_id, t.header, t.request_data, c.group_name from jk_testcase as t INNER JOIN jk_cgroups as c ON t.group_id = c.group_id  where t.group_id=%s or t.case_id=%s or t.case_name like %s limit %s, %s"
+    sql = "select t.sort, t.case_id, t.case_name, t.method, t.path, t.url, t.params, t.status, t.is_assert, t.is_rely_on, t.rely_id, t.header, t.request_data, c.group_name from jk_testcase as t INNER JOIN jk_cgroups as c ON t.group_id = c.group_id  where t.group_id=%s and t.case_id=%s and t.case_name like %s limit %s, %s"
     queryp = [group_name, case_id, ('%' + str(case_name) + '%'), page, limit, ]
     if case_id is None:
         if case_name is None:
@@ -682,8 +682,8 @@ def t_lists():
             logger.debug("select t.sort, t.case_id, t.case_name, t.method, t.path, t.url, t.params, t.status, t.is_assert, t.is_rely_on, t.rely_id, t.header, t.request_data, c.group_name from jk_testcase as t INNER JOIN jk_cgroups as c ON t.group_id = c.group_id  where t.group_id={}  limit {}, {}".format(group_name, page, limit))
             queryp = [group_name, page, limit, ]
         else:
-            sql = "select t.sort, t.case_id, t.case_name, t.method, t.path, t.url, t.params, t.status, t.is_assert, t.is_rely_on, t.rely_id, t.header, t.request_data, c.group_name from jk_testcase as t INNER JOIN jk_cgroups as c ON t.group_id = c.group_id  where t.group_id=%s or t.case_name like %s limit %s, %s"
-            logger.debug("select t.sort, t.case_id, t.case_name, t.method, t.path, t.url, t.params, t.status, t.is_assert, t.is_rely_on, t.rely_id, t.header, t.request_data, c.group_name from jk_testcase as t INNER JOIN jk_cgroups as c ON t.group_id = c.group_id  where t.group_id={} or t.case_name like {} limit {}, {}".format(group_name, ('%' + str(case_name) + '%'), page, limit))
+            sql = "select t.sort, t.case_id, t.case_name, t.method, t.path, t.url, t.params, t.status, t.is_assert, t.is_rely_on, t.rely_id, t.header, t.request_data, c.group_name from jk_testcase as t INNER JOIN jk_cgroups as c ON t.group_id = c.group_id  where t.group_id=%s and t.case_name like %s limit %s, %s"
+            logger.debug("select t.sort, t.case_id, t.case_name, t.method, t.path, t.url, t.params, t.status, t.is_assert, t.is_rely_on, t.rely_id, t.header, t.request_data, c.group_name from jk_testcase as t INNER JOIN jk_cgroups as c ON t.group_id = c.group_id  where t.group_id={} and t.case_name like {} limit {}, {}".format(group_name, ('%' + str(case_name) + '%'), page, limit))
             queryp = [group_name, ('%' + str(case_name) + '%'), page, limit, ]
     logger.debug("sql:" + sql + " " + "query:" + str(queryp))
     li = s.query_all(sql, queryp)
